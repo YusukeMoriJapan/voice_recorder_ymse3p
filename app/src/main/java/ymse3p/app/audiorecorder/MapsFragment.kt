f04e20d44d547a226e6be4e11f50fb033065c702
diffserve <currystew0826@gmail.com>
2021-04-17 08:47:36 +0900

    [add] add permissions(COARSE_LOCATION, BACKGROUND_LOCATION)

--- app/src/main/java/ymse3p/app/audiorecorder/MapsFragment.kt
+++ app/src/main/java/ymse3p/app/audiorecorder/MapsFragment.kt
@@ -8,11 +8,11 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
 import dagger.hilt.android.AndroidEntryPoint
 import ymse3p.app.audiorecorder.databinding.FragmentMapsBinding
 
@@ -20,21 +20,14 @@ import ymse3p.app.audiorecorder.databinding.FragmentMapsBinding
 class MapsFragment : Fragment() {
 
     private lateinit var _binding: FragmentMapsBinding
-    private val binding get() =  _binding
+    private val binding get() = _binding
 
     private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
         val sydney = LatLng(-34.0, 151.0)
         googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15f))
+        val newDecodedPath = mutableListOf<LatLng>()
+//        googleMap.addPolyline(PolylineOptions().addAll())
     }
 
     override fun onCreateView(
@@ -42,13 +35,13 @@ class MapsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentMapsBinding.inflate(inflater,container,false)
+        _binding = FragmentMapsBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-//        mapFragment?.getMapAsync(callback)
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
     }
 }
